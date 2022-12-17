4dc8f60269ee550e430b857670b4066b8f52b769
duonghuyom <huy.nguyen20@onemount.com>
2022-12-17 09:12:07 +0700

    encapsulation

---
+++ oop/encapsulation.ts
@@ -0,0 +1,4 @@
+// encapsulation co nghia la dong goi, cac phuong thuc, thuoc tinh se duoc dong goi thanh
+// nhung class, co the hinh dung mot vien thuoc co nhieu cac thanh phan ben trong
+
+// private variables can only be accessed within the same class (an outside class has no access to it)
