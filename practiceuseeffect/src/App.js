49b97d8fc4d94fab90f6753605e4319eaf89dd2a
duonghuyom <huy.nguyen20@onemount.com>
2023-02-27 08:41:37 +0700

    new code

--- practiceuseeffect/src/App.js
+++ practiceuseeffect/src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import './App.css';
 
-export default function App() {
+export default function App({initialCount}) {
   const [count, setCount] = useState(0);
 
   const increase = () => {
@@ -20,11 +20,18 @@ export default function App() {
     }, 2000);
   }
 
+
+
   return (
     <div>
       <p>You clicked {count} times</p>
       <button onClick={increase}>Click me</button>
       <button onClick={showAlert}>Show alert</button>
+      <br />
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
     </div>
   );
 }
